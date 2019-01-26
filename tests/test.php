09a1a43c6c0646291f8555f28d7cc570cb6df771
Mark Jansen <mark.jansen@reactos.org>
2019-01-26 18:02:50 +0100

    Remove test names (the names were not unique, thanks code coverage!)

--- tests/test.php
+++ tests/test.php
@@ -21,89 +21,89 @@ function update_config(&$config)
 update_config($config);
 
 $tests = array(
-    'InvalidToken #1' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Invalid token'),
         'post' => array(),
     ),
-    'InvalidIssueID #1' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Invalid issue ID'),
         'post' => array('token' => $config['jira_token']),
     ),
-    'InvalidIssueID #2' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Invalid issue ID'),
         'post' => array('token' => $config['jira_token'], 'text' => 'a'),
     ),
-    'InvalidIssueID #3' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Invalid issue ID'),
         'post' => array('token' => $config['jira_token'], 'text' => 'CORE-'),
     ),
-    'Valid #1' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Url requested by : https://jira.reactos.org/browse/ROSBE-1'),
         'post' => array('token' => $config['jira_token'], 'text' => 'ROSBE-1'),
     ),
-    'Valid #2' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Url requested by : https://jira.reactos.org/browse/CORE-1'),
         'post' => array('token' => $config['jira_token'], 'text' => 'CORE-1'),
     ),
-    'Valid #3' => array(
+    array(
         'name' => 'Jira',
         'expect' => array('text' => 'Url requested by test: https://jira.reactos.org/browse/CORE-1'),
         'post' => array('token' => $config['jira_token'], 'text' => 'CORE-1', 'user_name' => 'test'),
     ),
 
-    'InvalidToken #1' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => 'Invalid token'),
         'post' => array(),
     ),
-    'InvalidCommand #1' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => 'Invalid command'),
         'post' => array('token' => $config['translate_token'][0]),
     ),
-    'NotANumber #1' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => 'Not a number'),
         'post' => array('token' => $config['translate_token'][0]),
         'get' => array('cmd' => 'error'),
     ),
-    'NotANumber #2' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => 'Not a number'),
         'post' => array('token' => $config['translate_token'][0], 'text' => 'a'),
         'get' => array('cmd' => 'error'),
     ),
-    'Valid #1' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => "@, 0 could be:\n\tERROR_SUCCESS\n\tSTATUS_SUCCESS\n\tSTATUS_WAIT_0\n\tS_OK"),
         'post' => array('token' => $config['translate_token'][0], 'text' => '0'),
         'get' => array('cmd' => 'error'),
     ),
-    'Valid #2' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => "@, 0x0 could be:\n\tERROR_SUCCESS\n\tSTATUS_SUCCESS\n\tSTATUS_WAIT_0\n\tS_OK"),
         'post' => array('token' => $config['translate_token'][0], 'text' => '0x0'),
         'get' => array('cmd' => 'error'),
     ),
-    'Valid #3' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => "@test, 0xa could be:\n\tERROR_BAD_ENVIRONMENT\n\tSTATUS_WAIT_0 + 10"),
         'post' => array('token' => $config['translate_token'][0], 'text' => '0xa', 'user_name' => 'test'),
         'get' => array('cmd' => 'error'),
     ),
-    'InvalidCommand #2' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => "Invalid command"),
         'post' => array('token' => $config['translate_token'][0], 'text' => '0', 'user_name' => 'test'),
         'get' => array('cmd' => 'x'),
     ),
-    'Valid #3' => array(
+    array(
         'name' => 'Translate',
         'expect' => array('text' => "@test, 0 could be:\n\tWM_NULL"),
         'post' => array('token' => $config['translate_token'][0], 'text' => '0', 'user_name' => 'test'),
